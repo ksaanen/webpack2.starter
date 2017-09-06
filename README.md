b4aeb9e2ca0e9757e595e645c3f31b17caf00320
Kevin Saanen <ksaanen@independer.nl>
2017-09-06 16:11:48 +0200

    added seperate webpack configs

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 Base setup of TypeScript and webpack 2 for development.
 
 ## Usage
-- run "npm install" from project root directory.
-- run "npm start" from project root directory to start up the server
+- run "npm install" to install all dependancies.
+- run "npm start" to start up the dev server
+- run "npm build" 
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
+/*
+ Main application
+*/
+import * as dataModule from "./modules/data";
+import * as query from "./modules/data"; // extra line example
+
 (function(){
 
   'use strict';
 
-  let example:string = 'testing';
+  // example module import.
+  dataModule.getData();
 
+  console.log('bla');
+
+  let example:string = 'testing';
   document.body.innerHTML = example;
 
-})();  
+})();
